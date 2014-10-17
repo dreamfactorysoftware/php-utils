be6a556a8fa424cc004534f06e336d73454d87dc
Jerry Ablan <jerryablan@dreamfactory.com>
2014-10-17 15:41:40 -0400

    Composer dependencies updated.

---
+++ src/Includer.php
@@ -0,0 +1,30 @@
+<?php
+namespace DreamFactory\Library\Utility;
+
+/**
+ * Contains helpers to include/require PHP files
+ */
+class Includer
+{
+    /**
+     * Requires a file only if it exists
+     *
+     * @param string $file    the absolute /path/to/file.php
+     * @param bool   $require use "require" instead of "include"
+     * @param bool   $once    use "include_once" or "require_once" if $require is true
+     *
+     * @return bool|mixed
+     */
+    public static function includeIfExists( $file, $require = false, $once = false )
+    {
+        if ( file_exists( $file ) && is_readable( $file ) )
+        {
+            return $require
+                ? ( $once ? require_once( $file ) : require( $file ) )
+                : ( $once ? include_once( $file )
+                    : include( $file ) );
+        }
+
+        return false;
+    }
+}
