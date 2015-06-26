3d3483860fdb687936cb80f1a918ef3682317bc7
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-26 16:14:07 -0400

    Current work checkpoint. See diff for details.

--- src/Enums/FactoryEnum.php
+++ src/Enums/FactoryEnum.php
@@ -25,7 +25,7 @@ abstract class FactoryEnum
      */
     public function __invoke()
     {
-        return static::defines( '__default', true );
+        return static::defines('__default', true);
     }
 
     /**
@@ -35,20 +35,19 @@ abstract class FactoryEnum
      *
      * @return string
      */
-    public static function introspect( $class = null, array $seedConstants = array(), $overwrite = true )
+    public static function introspect($class = null, array $seedConstants = [], $overwrite = true)
     {
-        $_key = static::_cacheKey( $class );
+        $_key = static::_cacheKey($class);
 
-        if ( true === $overwrite || !isset( static::$_constants[$_key] ) )
-        {
-            $_mirror = new \ReflectionClass( $class ?: \get_called_class() );
+        if (true === $overwrite || !isset(static::$_constants[$_key])) {
+            $_mirror = new \ReflectionClass($class ?: \get_called_class());
 
             static::$_constants[$_key] = array_merge(
                 $seedConstants,
                 $_mirror->getConstants()
             );
 
-            unset( $_mirror );
+            unset($_mirror);
         }
 
         return $_key;
@@ -61,11 +60,11 @@ abstract class FactoryEnum
      *
      * @return string
      */
-    protected static function _cacheKey( $class = null )
+    protected static function _cacheKey($class = null)
     {
         static $_key = null;
 
-        return $_key ?: sha1( Inflector::neutralize( $class ?: \get_called_class() ) );
+        return $_key ?: sha1(Inflector::neutralize($class ?: \get_called_class()));
     }
 
     /**
@@ -76,9 +75,9 @@ abstract class FactoryEnum
      *
      * @return void
      */
-    public static function seedConstants( array $constants, $class = null )
+    public static function seedConstants(array $constants, $class = null)
     {
-        static::introspect( $class, $constants );
+        static::introspect($class, $constants);
     }
 
     /**
@@ -91,23 +90,21 @@ abstract class FactoryEnum
      *
      * @return array
      */
-    public static function getDefinedConstants( $flipped = false, $class = null, $listData = false )
+    public static function getDefinedConstants($flipped = false, $class = null, $listData = false)
     {
-        $_key = static::introspect( $class, array(), false );
+        $_key = static::introspect($class, [], false);
 
-        $_constants = false === $flipped ? static::$_constants[$_key] : array_flip( static::$_constants[$_key] );
+        $_constants = false === $flipped ? static::$_constants[$_key] : array_flip(static::$_constants[$_key]);
 
-        if ( false === $listData )
-        {
+        if (false === $listData) {
             return $_constants;
         }
 
-        $_temp = array();
+        $_temp = [];
 
-        foreach ( static::$_constants[$_key] as $_constant => $_value )
-        {
-            $_temp[$_value] = Inflector::display( Inflector::neutralize( $_constant ) );
-            unset( $_value, $_constant );
+        foreach (static::$_constants[$_key] as $_constant => $_value) {
+            $_temp[$_value] = Inflector::display(Inflector::neutralize($_constant));
+            unset($_value, $_constant);
         }
 
         return $_temp;
@@ -127,11 +124,10 @@ abstract class FactoryEnum
      *
      * @return bool
      */
-    public static function contains( $value, $returnConstant = false )
+    public static function contains($value, $returnConstant = false)
     {
-        if ( in_array( $value, static::getDefinedConstants() ) )
-        {
-            return $returnConstant ? static::nameOf( $value ) : true;
+        if (in_array($value, static::getDefinedConstants())) {
+            return $returnConstant ? static::nameOf($value) : true;
         }
 
         return false;
@@ -154,22 +150,19 @@ abstract class FactoryEnum
      * @throws \InvalidArgumentException
      * @return bool
      */
-    public static function defines( $constant, $returnValue = false )
+    public static function defines($constant, $returnValue = false)
     {
         $_constants = static::getDefinedConstants();
 
-        if ( false === ( $_has = array_key_exists( $constant, $_constants ) ) )
-        {
-            if ( false !== ( $_has = array_key_exists( strtoupper( $constant ), $_constants ) ) )
-            {
+        if (false === ($_has = array_key_exists($constant, $_constants))) {
+            if (false !== ($_has = array_key_exists(strtoupper($constant), $_constants))) {
                 $_has = true;
-                $constant = strtoupper( $constant );
+                $constant = strtoupper($constant);
             }
         }
 
-        if ( !$_has && $returnValue )
-        {
-            throw new \InvalidArgumentException( 'The constant "' . $constant . '" is not defined.' );
+        if (!$_has && $returnValue) {
+            throw new \InvalidArgumentException('The constant "' . $constant . '" is not defined.');
         }
 
         return !$returnValue ? $_has : $_constants[$constant];
@@ -179,43 +172,38 @@ abstract class FactoryEnum
      * Returns the constant name as a string
      *
      * @param string|int $constant The CONSTANT's value that you want the name of
-     * @param bool       $flipped  If false, $constant should be the CONSTANT's name. The CONSTANT's value will be returned instead.
-     * @param bool       $pretty   If true, returned value is prettified (acme.before_event becomes "Acme Before Event")
+     * @param bool       $flipped  If false, $constant should be the CONSTANT's name. The CONSTANT's value will be
+     *                             returned instead.
+     * @param bool       $pretty   If true, returned value is prettified (acme.before_event becomes "Acme Before
+     *                             Event")
      *
      * @throws \InvalidArgumentException
      * @return string|int
      */
-    public static function nameOf( $constant, $flipped = true, $pretty = true )
+    public static function nameOf($constant, $flipped = true, $pretty = true)
     {
         $_result = null;
 
-        foreach ( static::getDefinedConstants() as $_name => $_value )
-        {
-            if ( $flipped )
-            {
-                if ( $_name == $constant )
-                {
+        foreach (static::getDefinedConstants() as $_name => $_value) {
+            if ($flipped) {
+                if ($_name == $constant) {
                     $_result = $_value;
                     break;
                 }
-            }
-            else
-            {
-                if ( $_value == $constant )
-                {
+            } else {
+                if ($_value == $constant) {
                     $_result = $_name;
                     break;
                 }
             }
         }
 
-        if ( !$_result )
-        {
-            throw new \InvalidArgumentException( 'A constant with the value of "' . $constant . '" does not exist.' );
+        if (!$_result) {
+            throw new \InvalidArgumentException('A constant with the value of "' . $constant . '" does not exist.');
         }
 
         return !$flipped && $pretty
-            ? Inflector::display( Inflector::neutralize( $_result ) )
+            ? Inflector::display(Inflector::neutralize($_result))
             : $_result;
     }
 
@@ -225,8 +213,34 @@ abstract class FactoryEnum
      *
      * @return string
      */
-    public static function prettyNameOf( $constant, $flipped = true )
+    public static function prettyNameOf($constant, $flipped = true)
     {
-        return static::nameOf( $constant, $flipped, true );
+        return static::nameOf($constant, $flipped, true);
+    }
+
+    /**
+     * Returns a list of the constants in a comma-separated display manner
+     *
+     * @param string|null $quote An optional quote to enrobe the value (' or " only)
+     *
+     * @return string
+     */
+    public static function prettyList($quote = null)
+    {
+        $_list = null;
+        $quote != '\'' && $quote != '"' && $quote = null;
+        $_values = array_values(static::getDefinedConstants(true));
+
+        for ($_i = 0, $_max = count($_values); $_i < $_max; $_i++) {
+            //  Skip constants starting with an underscore
+            if ($_values[$_i][0] == '_') {
+                continue;
+            }
+
+            $_i != 0 && $_list .= ', ';
+            $_list .= $quote . strtolower($_values[$_i]) . $quote;
+        }
+
+        return $_list;
     }
 }
