234bf1bf76503bbc8bdf1f946480def89d2bbe8e
Jerry Ablan <jerryablan@dreamfactory.com>
2015-08-28 12:11:57 -0400

    Added new Flasher utility class

---
+++ src/Flasher.php
@@ -0,0 +1,117 @@
+<?php namespace DreamFactory\Library\Utility;
+
+use Illuminate\Support\Facades\Session;
+
+/**
+ * Helpers for UI session flash messages
+ */
+class Flasher
+{
+    //******************************************************************************
+    //* Members
+    //******************************************************************************
+
+    /**
+     * @type string The prefix for flash keys
+     */
+    protected static $prefix;
+    /**
+     * @type string The separator between key segments
+     */
+    protected static $separator = '.';
+
+    //******************************************************************************
+    //* Methods
+    //******************************************************************************
+
+    /**
+     * @param string      $value
+     * @param bool|true   $success
+     * @param string|null $key If specified, use as flash key
+     */
+    public static function flash($value, $success = true, $key = null)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        Session::flash($key ?: static::buildFlashKey($success), $value);
+    }
+
+    /**
+     * Flashes a message ONLY if the key is empty
+     *
+     * @param string      $value
+     * @param bool        $success
+     * @param string|null $key If specified, use as flash key
+     *
+     * @internal param string $message
+     */
+    public static function flashIf($value, $success = true, $key = null)
+    {
+        $_key = $key ?: static::buildFlashKey($success);
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        if (!Session::has($_key)) {
+            static::flash($value, $success, $_key);
+        }
+    }
+
+    /**
+     * Clear the flash
+     *
+     * @param bool|null $success Which key to clear, null for both (default)
+     */
+    public static function forget($success = null)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $_keys =
+            null === $success ? [static::buildFlashKey(true), static::buildFlashKey(false)]
+                : [static::buildFlashKey($success)];
+
+        foreach ($_keys as $_key) /** @noinspection PhpUndefinedMethodInspection */ {
+            Session::forget($_key);
+        }
+    }
+
+    /**
+     * Constructs the complete flash key
+     *
+     * @param bool|true $success
+     *
+     * @return string
+     */
+    protected static function buildFlashKey($success = true)
+    {
+        return (static::$prefix ? static::$prefix . static::$separator : null) . ($success ? 'success' : 'failure');
+    }
+
+    /**
+     * @return string
+     */
+    public static function getPrefix()
+    {
+        return static::$prefix;
+    }
+
+    /**
+     * @param string $prefix
+     */
+    public static function setPrefix($prefix)
+    {
+        static::$prefix = $prefix;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getSeparator()
+    {
+        return static::$separator;
+    }
+
+    /**
+     * @param string $separator
+     */
+    public static function setSeparator($separator)
+    {
+        static::$separator = $separator;
+    }
+}
