3be35a1f3f940f5bde64c497cc6d4fbc60800477
Jerry Ablan <jerryablan@dreamfactory.com>
2016-01-08 01:47:11 -0500

    XML conversion fixes

--- src/Shapers/XmlShape.php
+++ src/Shapers/XmlShape.php
@@ -1,8 +1,6 @@
 <?php namespace DreamFactory\Library\Utility\Shapers;
 
 use DreamFactory\Library\Utility\Interfaces\ShapesData;
-use DreamFactory\Library\Utility\Scalar;
-use Illuminate\Support\Arr;
 
 class XmlShape implements ShapesData
 {
@@ -28,16 +26,24 @@ class XmlShape implements ShapesData
     //******************************************************************************
 
     /**
-     * Transforms an array of data into a new shape
+     * XmlShape constructor
      *
-     * @param array $source  The source data
-     * @param array $options Any options to pass through to the shaping mechanism
-     *
-     * @return mixed
+     * @param string      $version
+     * @param string      $encoding
+     * @param string|null $root Optional root node
+     * @param bool        $pretty
      */
-    public static function transform(array $source, $options = [])
+    public function __construct($version = '1.0', $encoding = 'UTF-8', $root = null, $pretty = true)
     {
-        return static::make(array_get($options, 'root', 'root'), $source, $options)->saveXml();
+        $_xmlString = '<?xml version="' .
+            $version .
+            '" encoding="' .
+            $encoding .
+            '"?>' .
+            ($root ? '<' . $root . '></' . $root . '>' : '<root></root>');
+
+        $this->document = new \SimpleXMLElement($_xmlString);
+        $this->pretty = $pretty;
     }
 
     /**
@@ -51,125 +57,46 @@ class XmlShape implements ShapesData
      */
     public static function make($root, array $array = [], $options = [])
     {
-        $_xml = new static(array_get($options, 'version', '1.0'), array_get($options, 'encoding', 'UTF-8'), array_get($options, 'pretty', true));
-        $_xml->appendNode($root, $array);
+        $_xml =
+            new static(array_get($options, 'version', '1.0'),
+                array_get($options, 'encoding', 'UTF-8'),
+                array_get($options, 'root'),
+                array_get($options, 'pretty', true));
 
-        return $_xml;
+        return $_xml->addChildArray($root, $array);
     }
 
     /**
-     * XmlShape constructor
+     * Transforms an array of data into a new shape
      *
-     * @param string $version
-     * @param string $encoding
-     * @param bool   $pretty
+     * @param array $source  The source data
+     * @param array $options Any options to pass through to the shaping mechanism
+     *
+     * @return mixed
      */
-    public function __construct($version = '1.0', $encoding = 'UTF-8', $pretty = true)
+    public static function transform(array $source, $options = [])
     {
-        $this->pretty = $pretty;
-        $this->document = new \DOMDocument($version, $this->encoding = $encoding);
-        $this->document->formatOutput = $pretty;
+        return static::make(null, $source, $options)->document->saveXML();
     }
 
     /**
-     * Convert an Array to XML
-     *
-     * @param string           $root  The name of the root node
-     * @param array|string|int $array The data to reshape
-     *
-     * @return \DOMElement
+     * @param array         $array
+     * @param \DOMNode|null $node
      */
-    protected function convert($root, $array = null)
+    protected function appendArray(array $array, &$node = null)
     {
-        $_node = $this->document->createElement($root);
+        $node = $node ?: $this->document;
 
-        //  Get attributes
-        if (is_array($array)) {
-            if (array_key_exists('@attributes', $array)) {
-                foreach ($array['@attributes'] as $_key => $_value) {
-                    if (!$this->isValidTagName($_key)) {
-                        throw new \RuntimeException('Illegal character in attribute name. attribute: ' . $_key . ' in node: ' . $root);
-                    }
+        foreach ($array as $_key => $_value) {
+            $_key = is_numeric($_key) ? 'item' . $_key : $_key;
 
-                    $_node->setAttribute($_key, Scalar::boolval($_value));
-                }
+            if (is_array($_value)) {
+                $_node = $node->addChild($_key);
+                $this->appendArray($_value, $_node);
+            } else {
+                $node->addChild($_key, $_value);
             }
-
-            //  Check for text/data
-            if (array_key_exists('@value', $array)) {
-                $_node->appendChild($this->document->createTextNode(Scalar::boolval($array['@value'])));
-
-                return $_node;
-            }
-
-            //  Check for CDATA
-            if (array_key_exists('@data', $array)) {
-                $_node->appendChild($this->document->createCDATASection(Scalar::boolval($array['@cdata'])));
-
-                return $_node;
-            }
-
-            //  Build out the xml tree
-            foreach ($array as $_key => $_value) {
-                if (!$this->isValidTagName($_key)) {
-                    throw new \RuntimeException('Illegal character in tag name. tag: ' . $_key . ' in node: ' . $root);
-                }
-
-                //  Non-scalar values require more attention
-                if (!is_scalar($_value)) {
-                    if (is_array($_value)) {
-                        if (!Arr::isAssoc($_value)) {
-                            foreach ($_value as $_subnode) {
-                                $_node->appendChild($this->convert($_key, $_subnode));
-                            }
-                        } else {
-                            $_node->appendChild($this->convert($_key, $_value));
-                        }
-
-                        return $_node;
-                    }
-
-                    if (is_object($_value)) {
-                        try {
-                            if (method_exists($_value, 'toArray')) {
-                                $_value = $_value->toArray();
-                            } else {
-                                $_value = (array)$_value;
-                            }
-                        } catch (\Exception $_ex) {
-                            throw new \RuntimeException('It is not possible to convert the output  to XML.');
-                        }
-                    }
-                }
-
-                $_node->appendChild($this->convert($_key, $_value));
-            }
-        } else {
-            $_node->appendChild($this->document->createTextNode(Scalar::boolval($array)));
         }
-
-        return $_node;
-    }
-
-    /*
-     * Check if the tag name or attribute name contains illegal characters
-     * Ref: http://www.w3.org/TR/xml/#sec-common-syn
-     */
-    protected function isValidTagName($tag)
-    {
-        static $_pattern = '/^[a-z_]+[a-z0-9:\-\._]*[^:]*$/i';
-
-        return preg_match($_pattern, $tag, $matches) && $tag == $matches[0];
-    }
-
-    /**
-     * @param \DomNode $node
-     *
-     * @return \DOMNode
-     */
-    public function appendChild($node)
-    {
-        return $this->document->appendChild($node);
     }
 
     /**
@@ -177,20 +104,34 @@ class XmlShape implements ShapesData
      *
      * @param string $root
      * @param array  $array
+     *
+     * @return $this
      */
-    public function appendNode($root, $array = [])
+    public function addChildArray($root, $array = [])
     {
-        $this->appendChild($this->convert($root, $array));
+        $_node = $this->document->addChild($root);
+        $this->appendArray($array, $_node);
+
+        return $this;
     }
 
     /**
-     * @param \DomNode|null $node
-     * @param int|null      $options
-     *
-     * @return string
+     * @param \SimpleXMLElement $node
+     * @param array             $array
      */
-    public function saveXml($node = null, $options = null)
+    public static function fromArray(&$node, array $array)
     {
-        return $this->document->saveXML($node, $options);
+        $node = $node ?: new \DOMNode();
+
+        foreach ($array as $_key => $_value) {
+            is_numeric($_key) && $_key = 'item' . $_key;
+
+            if (is_array($_value)) {
+                $_subnode = $node->addChild($_key);
+                static::fromArray($_subnode, $_value);
+            } else {
+                $node->addChild($_key, $_value);
+            }
+        }
     }
 }
